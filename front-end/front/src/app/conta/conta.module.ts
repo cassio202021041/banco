94d28023646f7537817778def00ce99d17d61292
cassio202021041 <cassio00710@hotmail.com>
2024-09-01 15:08:31 -0300

    Commit Componets
    Criação components crud

--- front-end/front/src/app/conta/conta.module.ts
+++ front-end/front/src/app/conta/conta.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
+import { ViewComponent } from './view/view.component';
+import { IndexComponent } from './index/index.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CreateComponent,
+    EditComponent,
+    ViewComponent,
+    IndexComponent
+  ],
   imports: [
     CommonModule
   ]
