a51e99149eb091d73a2270b521d3634b44a1baef
cassio202021041 <cassio00710@hotmail.com>
2024-09-01 15:52:00 -0300

    configuração componente edit
    configuração componente edit

--- front-end/front/src/app/conta/edit/edit.component.ts
+++ front-end/front/src/app/conta/edit/edit.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from 'express';
+import { Conta } from '../conta';
+import { ContaService } from '../conta.service';
 
 @Component({
   selector: 'app-edit',
@@ -6,5 +11,35 @@ import { Component } from '@angular/core';
   styleUrl: './edit.component.css'
 })
 export class EditComponent {
+  id!:number;
+  conta!:Conta;
+  form!:FormGroup;
+
+  constructor(public contaService:ContaService, private router:Router, private route:ActivatedRoute){
+
+  }
+
+  ngOnInit():void{
+    this.id = this.route.snapshot.params['contaId'];
+    this.contaService.find(this.id).subscribe((data:Conta)=>{
+      this.conta = data;
+    });
+
+    this.form = new FormGroup({
+      title: new FormControl('', [Validators.required]),
+      body: new FormControl('', Validators.required)
+    })
+  }
+
+  get f(){
+    return this.form.controls;
+  }
+  submit(){
+    console.log(this.form.value)
+    this.contaService.update(this.id,this.form.value).subscribe((res:any)=>{
+      console.log("data updated successfully !");
+      this.router.navigateByUrl('conta/index')
+    })
+  }
 
 }
