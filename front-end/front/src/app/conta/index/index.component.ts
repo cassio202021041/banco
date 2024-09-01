40e7ea4a8f6e23b8f2abd65792d10415b3c9f255
cassio202021041 <cassio00710@hotmail.com>
2024-09-01 16:07:11 -0300

    Configuração component index
    Configuração component index

--- front-end/front/src/app/conta/index/index.component.ts
+++ front-end/front/src/app/conta/index/index.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Conta } from '../conta';
+import { ContaService } from '../conta.service';
 
 @Component({
   selector: 'app-index',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './index.component.css'
 })
 export class IndexComponent {
+  contas:Conta[]=[];
 
+  constructor(public contaService:ContaService){}
+
+  ngOnInit():void{
+    this.contaService.getAll().subscribe((data:Conta[])=>{
+      this.contas = data;
+      console.log(this.contas);
+    })
+  }
+
+  deleteConta(id:number){
+    this.contaService.delete(id).subscribe(res =>{
+      this.contas = this.contas.filter(item=>item.id !==id);
+      console.log("conta Deleted Successsfully !")
+    })
+  }
 }
+
